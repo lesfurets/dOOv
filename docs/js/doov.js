3118aa5dc222be63d6bb035050ad0717efb72a66
Alexandre DuBreuil <dubreuil.alex@gmail.com>
2018-01-19 10:37:10 +0100

    Correct slides for snowcamp

--- docs/js/doov.js
+++ docs/js/doov.js
@@ -1,5 +1,13 @@
+const operators = ['or', 'and', 'after', 'before', 'eq', 'ageAt', 'greaterOrEquals', 'greaterThan', 'lesserOrEquals', 'lesserThan', 'startsWith', 'matches']
+
 const doov = function (args) {
-  //alert(args);
+  $('.prettyprinted .pln').each(function () {
+    if ($(this).text().trim() == 'DOOV') {
+      $(this).addClass('doov');
+    } else if (operators.includes($(this).text().trim())) {
+      $(this).addClass('operator');
+    }
+  });
 }
 
 window.exports = { 'doov': doov };
